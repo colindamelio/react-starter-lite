b7d629fd4993cf91b922968e9e1bcc68ebe62c82
colindamelio <colin.damelio@gmail.com>
2017-02-19 20:59:09 -0500

    fooling around with state

--- app/components/homepage/index.js
+++ app/components/homepage/index.js
@@ -4,12 +4,32 @@ import Contact from '../contacts';
 import userData from '../../data';
 
 class Homepage extends React.Component {
+  componentWillMount() {
+    this.setState({
+      contacts: userData,
+    });
+  }
+
+  handleAddContact = (e) => {
+    const contacts = this.state.contacts;
+    const newId = contacts[contacts.length - 1].id + 1;
+
+    e.preventDefault();
+    this.setState({
+      contacts: contacts.concat({ id: newId, name: `New Contact # ${newId}` }),
+    });
+  };
+
+
   render() {
     return (
       <div id="homepage">
-        {userData.map(contact =>
-          <Contact key={contact.id} name={contact.name} email={contact.email} />, // {...info}
-        )}
+        <button onClick={this.handleAddContact}>Click To Add More</button>
+        <div className="contact-list">
+          {this.state.contacts.map(contact =>
+            <Contact key={contact.id} name={contact.name} {...contact} />,
+          )}
+        </div>
       </div>
     );
   }
